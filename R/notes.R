5b71945596aefba4b6ab8925351b3f6cf927b264
dcossyleon <dcossyleon@gmail.com>
2019-06-06 12:21:26 -0400

    Adds many more files after following usethis tutorial

---
+++ R/notes.R
@@ -0,0 +1,8 @@
+create_package("~/Documents/New R Projects/firstclass")
+use_git()
+use_github()
+
+
+use_travis()
+use_appveyor()
+use_coverage(type = c("codecov"))
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(firstclass)
+
+test_check("firstclass")
