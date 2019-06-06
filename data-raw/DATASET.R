c87b295e199fcb2e23111e69e2e9da3316bb9cc9
dcossyleon <dcossyleon@gmail.com>
2019-06-06 16:43:40 -0400

    Adds dataset file to man folder

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,5 +6,5 @@ aggression_behavior <- readr::read_csv(here::here("data-raw", "aggression_behavi
 # end of exports
 usethis::use_data(aggression_behavior, overwrite = TRUE) # MOST CRITICAL STEP!
 
-
+usethis::use_r(aggression_behavior) # writes corresponding file to R folder #Error?? Is this the right place to put this?
 
